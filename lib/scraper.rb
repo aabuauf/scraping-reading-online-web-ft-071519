1296776a7e5a1c3e95fc6c01215126c7f01f1416
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-30 13:16:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
-
-html = open("https://flatironschool.com/")
-
-doc = Nokogiri::HTML(html)
-
+ 
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
 doc.css(".site-header__hero__headline").text
 
