9b2105762acdce97bcae57d1fa4782392ed46d31
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-30 14:06:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,5 +2,9 @@ require 'nokogiri'
 require 'open-uri'
  
 doc = Nokogiri::HTML(open("http://flatironschool.com/"))
-puts doc.css(".site-header__hero__headline")
+courses = doc.css(".tout__label.heading.heading--level-4")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
 
